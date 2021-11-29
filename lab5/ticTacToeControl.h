0b4f396abd1d31afb80084459ab7f5a8a26744e3
jones166 <92109028+jones166@users.noreply.github.com>
2021-11-29 09:40:05 -0700

    Initialize git_demo

---
+++ lab5/ticTacToeControl.h
@@ -0,0 +1,21 @@
+/*
+This software is provided for student assignment use in the Department of
+Electrical and Computer Engineering, Brigham Young University, Utah, USA.
+
+Users agree to not re-host, or redistribute the software, in source or binary
+form, to other persons or other institutions. Users may modify and use the
+source code for personal or educational use.
+
+For questions, contact Brad Hutchings or Jeff Goeders, https://ece.byu.edu/
+*/
+
+#ifndef TICTACTOECONTROL_H
+#define TICTACTOECONTROL_H
+
+// Tick the tic-tac-toe conroller state machine
+void ticTacToeControl_tick();
+
+// Initialize the tic-tac-toe conroller state machine
+void ticTacToeControl_init();
+
+#endif /* TICTACTOECONTROL_H */
